ba3eb7ab8f0a8382734e345ed8d86fa68a172f0f
jonathan <99052897@mydavinci.nl>
2022-06-20 11:29:25 +0200

    Initial commit

---
+++ shopping-cart/resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <div class="nav-item">
+                <a class="nav-link" href="#">Shopping cart</a>
+            </div>
+        <div  class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Account
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">instellingen</a>
+                    <a class="dropdown-item" href="#">logout</a>
+                </div>
+        </div>
+    </div>
+</nav>
---
+++ shopping-cart/resources/views/shop/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.master')
+
+@section('title')
+    Shopping Cart
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-6 col-md-4 border">
+            <div class="thumbnail">
+                <img src="https://cdn.webshopapp.com/shops/297404/files/350382788/800x1024x1/glock-17-gen4.jpg" class="img-responsive" alt="...">
+                <div class="caption">
+                    <h3>product title</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis cupiditate debitis dolor dolore eos incidunt ipsum iure maxime minima modi nesciunt nobis odio quis reiciendis sapiente, tenetur? Aspernatur, unde.</p>
+                    <div>
+                        <a href="#" class="btn btn-primary float-right" role="button">Button</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
