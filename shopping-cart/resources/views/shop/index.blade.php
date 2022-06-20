3af83cfb54fac84ce5cd3f2cd53b63acca845855
jonathan <99052897@mydavinci.nl>
2022-06-20 12:19:34 +0200

    begin aan de layout en voorbereiding database connection

--- shopping-cart/resources/views/shop/index.blade.php
+++ shopping-cart/resources/views/shop/index.blade.php
@@ -11,9 +11,72 @@
                 <img src="https://cdn.webshopapp.com/shops/297404/files/350382788/800x1024x1/glock-17-gen4.jpg" class="img-responsive" alt="...">
                 <div class="caption">
                     <h3>product title</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis cupiditate debitis dolor dolore eos incidunt ipsum iure maxime minima modi nesciunt nobis odio quis reiciendis sapiente, tenetur? Aspernatur, unde.</p>
-                    <div>
-                        <a href="#" class="btn btn-primary float-right" role="button">Button</a>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis cupiditate debitis dolor dolore eos incidunt ipsum iure maxime minima modi nesciunt nobis odio quis reiciendis sapiente, tenetur? Aspernatur, unde.</p>
+                    <div class="footer">
+                        <div class="float-left price">€100</div>
+                        <a href="#" class="btn btn-success float-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div><div class="col-sm-6 col-md-4 border">
+            <div class="thumbnail">
+                <img src="https://cdn.webshopapp.com/shops/297404/files/350382788/800x1024x1/glock-17-gen4.jpg" class="img-responsive" alt="...">
+                <div class="caption">
+                    <h3>product title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis cupiditate debitis dolor dolore eos incidunt ipsum iure maxime minima modi nesciunt nobis odio quis reiciendis sapiente, tenetur? Aspernatur, unde.</p>
+                    <div class="footer">
+                        <div class="float-left price">€100</div>
+                        <a href="#" class="btn btn-success float-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div><div class="col-sm-6 col-md-4 border">
+            <div class="thumbnail">
+                <img src="https://cdn.webshopapp.com/shops/297404/files/350382788/800x1024x1/glock-17-gen4.jpg" class="img-responsive" alt="...">
+                <div class="caption">
+                    <h3>product title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis cupiditate debitis dolor dolore eos incidunt ipsum iure maxime minima modi nesciunt nobis odio quis reiciendis sapiente, tenetur? Aspernatur, unde.</p>
+                    <div class="footer">
+                        <div class="float-left price">€100</div>
+                        <a href="#" class="btn btn-success float-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><div class="row">
+        <div class="col-sm-6 col-md-4 border">
+            <div class="thumbnail">
+                <img src="https://cdn.webshopapp.com/shops/297404/files/350382788/800x1024x1/glock-17-gen4.jpg" class="img-responsive" alt="...">
+                <div class="caption">
+                    <h3>product title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis cupiditate debitis dolor dolore eos incidunt ipsum iure maxime minima modi nesciunt nobis odio quis reiciendis sapiente, tenetur? Aspernatur, unde.</p>
+                    <div class="footer">
+                        <div class="float-left price">€100</div>
+                        <a href="#" class="btn btn-success float-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div><div class="col-sm-6 col-md-4 border">
+            <div class="thumbnail">
+                <img src="https://cdn.webshopapp.com/shops/297404/files/350382788/800x1024x1/glock-17-gen4.jpg" class="img-responsive" alt="...">
+                <div class="caption">
+                    <h3>product title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis cupiditate debitis dolor dolore eos incidunt ipsum iure maxime minima modi nesciunt nobis odio quis reiciendis sapiente, tenetur? Aspernatur, unde.</p>
+                    <div class="footer">
+                        <div class="float-left price">€100</div>
+                        <a href="#" class="btn btn-success float-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div><div class="col-sm-6 col-md-4 border">
+            <div class="thumbnail">
+                <img src="https://cdn.webshopapp.com/shops/297404/files/350382788/800x1024x1/glock-17-gen4.jpg" class="img-responsive" alt="...">
+                <div class="caption">
+                    <h3>product title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis cupiditate debitis dolor dolore eos incidunt ipsum iure maxime minima modi nesciunt nobis odio quis reiciendis sapiente, tenetur? Aspernatur, unde.</p>
+                    <div class="footer">
+                        <div class="float-left price">€100</div>
+                        <a href="#" class="btn btn-success float-right" role="button">Add to Cart</a>
                     </div>
                 </div>
             </div>
